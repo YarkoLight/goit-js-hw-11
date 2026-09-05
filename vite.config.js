b2972f5f873ae07391d1db77b5ddda44bfc5dbda
Yarko <lavi.rise@gmail.com>
2026-09-05 09:38:43 +0300

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
+    base: command === 'serve' ? '/' : '/goit-js-hw-11/',
     build: {
       sourcemap: true,
       rollupOptions: {
